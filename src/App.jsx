3ad96ba07d6be4809996853865ba8c2dc38144ec
Al101020 <alex.vl.m@yandex.ru>
2025-11-17 10:05:47 +1000

    commit-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
+import ReactDOM from 'react';
 import './App.css'
 
+const Like = ({ count, onLike }) => {
+  return (
+    <button className="heartT" onClick={onLike}>
+      {count} 
+    </button>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [isActive, setActive] = useState(false);
+  const onLike = () => setActive(!isActive);
+
+  const [count, setCount] = useState(0);
+  const onLikeT = () => {
+    console.log('LIKE!', count);
+    setCount(count + 1);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button className={isActive ? 'heart heart_active' : 'heart'} onClick={onLike} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>---------- -------- ---------</div>
+      <h1>Ку-Ку</h1>
+      <div>---------- -------- ---------</div>
+      <div>
+        <Like count={count} onLike={onLikeT}/>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
